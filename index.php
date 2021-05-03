df9609070678de647479a5b6a34cc83816e1bf34
Alberto Gambino <gambafb@libero.it>
2021-05-03 18:41:04 +0100

    esercizio aggiornato

--- index.php
+++ index.php
@@ -16,29 +16,52 @@
 
     <h1>Hello PHP</h1>
 
+    <h2>Frase Originale</h2>
+
+    <h4>
+
+      <?php
+
+      $frase = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br>';
+
+      echo $frase;
+
+      ?>
+
+    </h4>
+
+    <h2>Lunghezza frase</h2>
+
+    <h4>
+
+      <?php
+
+      $frase = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br>';
+
+      echo strlen($frase);
+
+       ?>
 
 
-    <?php
+    </h4>
 
-    // phpinfo();
+    <h2>Frase Censurata</h2>
 
-    $frase = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br>';
+    <h4>
 
-    $replace = $_GET['badword'];
+      <?php
 
-    $modificata = str_replace($replace, '***', $frase);
+      $frase = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br>';
 
-    echo $frase;
+      $replace = $_GET['badword'];
 
-    echo strlen($frase);
+      $modificata = str_replace($replace, '***', $frase);
 
-    echo $modificata;
+      echo $modificata;
 
+       ?>
 
-
-    ?>
-
-
+    </h4>
 
   </body>
 </html>
